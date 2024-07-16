956bd9c273fbca81626bda877bda340964846eca
Jenna Jones <jljones121@gmail.com>
2024-07-16 15:51:12 -0500

    '__init__.py' lines 13-17 errors rmved

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,11 +10,8 @@ from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
-<<<<<<< HEAD
-# from api.v1.views.users import *
-=======
 from api.v1.views.users import *
->>>>>>> 55cb32533fccc572c8adac4881d3216310b516d6
 from api.v1.views.places import *
 # from api.v1.views.places_reviews import *
-#from api.v1.views.places_amenities import *
+# from api.v1.views.places_amenities import *
+# from api.v1.views.places_users import *
