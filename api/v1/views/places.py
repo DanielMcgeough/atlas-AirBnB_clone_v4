9ed10946f3f0c9e4c2363212f9ff96075fb26c76
Jenna Jones <jljones121@gmail.com>
2024-07-16 16:28:15 -0500

    places.py, updated create_place from 415 to 400 line 53

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -10,6 +10,7 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
+
 @app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
 def get_places(city_id):
     """ Retrieves the list of all Place objects of a City """
@@ -21,6 +22,7 @@ def get_places(city_id):
     places = [place.to_dict() for place in city.places]
     return jsonify(places)
 
+
 @app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
 def get_place(place_id):
     """ Retrieves a Place object """
@@ -30,6 +32,7 @@ def get_place(place_id):
         logging.error("Place not found")
         abort(404)
     return jsonify(place.to_dict())
+
 
 @app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
 def delete_place(place_id):
@@ -44,31 +47,30 @@ def delete_place(place_id):
     logging.debug(f"Place with place_id: {place_id} deleted successfully")
     return jsonify({}), 200
 
+
 @app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
 def create_place(city_id):
     """ Creates a Place """
     logging.debug(f"Creating place in city with city_id: {city_id}")
-    
-    # Check if Content-Type is application/json
     if not request.is_json:
-        logging.error("Unsupported Media Type - Expecting application/json")
-        abort(415, "Unsupported Media Type - Expecting application/json")
-    
+        logging.error("Bad Request")
+        abort(400, "Bad Request")
+
     city = storage.get(City, city_id)
     if not city:
         logging.error("City not found")
         abort(404)
-    
+
     try:
         data = request.get_json(force=True)
     except Exception as e:
         logging.error(f"Invalid JSON: {e}")
         abort(400, "Not a JSON")
-    
+
     if 'user_id' not in data:
         logging.error("Missing user_id")
         abort(400, "Missing user_id")
-    
+
     user = storage.get(User, data['user_id'])
     if not user:
         logging.error("User not found")
@@ -76,12 +78,13 @@ def create_place(city_id):
     if 'name' not in data:
         logging.error("Missing name")
         abort(400, "Missing name")
-    
+
     data['city_id'] = city_id
     place = Place(**data)
     place.save()
     logging.debug(f"Place created with data: {data}")
     return jsonify(place.to_dict()), 201
+
 
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
 def update_place(place_id):
