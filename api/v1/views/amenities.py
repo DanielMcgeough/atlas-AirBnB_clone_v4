d08248781ab455d4649c29f4b7120ace68023664
bonxbons <144151209+bonxbons@users.noreply.github.com>
2024-07-16 15:03:45 -0500

    writing my script again

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,1 +1,9 @@
+#!/usr/bin/python3
+"""
+Creates a new view for Amenity objects that handles all default RESTFul API actions
+"""
+
+from flask import jsonify, request, abort
+from models import storage
+from api.v1.views import app_views
 
