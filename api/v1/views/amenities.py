5b900f99050651b91e43f409b1e176be075f7f7b
bonxbons <144151209+bonxbons@users.noreply.github.com>
2024-07-16 15:12:33 -0500

    finish amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -30,3 +30,29 @@ def delete_amenity(amenity_id):
     storage.delete(amenity)
    return jsonify({}), 200
 
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+def create_amenity():
+    """Creates a Amenity"""
+    data = request.get_json()
+    if data is None:
+        abort(400, description='Not a JSON')
+    if 'name' not in data:
+        abort(400, description='Missing name')
+    new_amenity = storage.new('Amenity', **data)
+    storage.save()
+    return jsonify(new_amenity.to_dict()), 201
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+def update_amenity(amenity_id):
+    """Updates a Amenity object"""
+    amenity = storage.get('Amenity', amenity_id)
+    if amenity is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, description='Not a JSON')
+    for key, value in data.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(amenity, key, value)
+    storage.save()
+    return jsonify(amenity.to_dict()), 200
