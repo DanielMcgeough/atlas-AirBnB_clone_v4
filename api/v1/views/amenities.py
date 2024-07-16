026227cb337c92b857f4a739011f02cb0d0e12da
bonxbons <144151209+bonxbons@users.noreply.github.com>
2024-07-16 15:09:26 -0500

    adding function

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -7,3 +7,26 @@ from flask import jsonify, request, abort
 from models import storage
 from api.v1.views import app_views
 
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+def get_all_amenities():
+    """Retrieves the list of all Amenity objects"""
+    all_amenities = storage.all('Amenity').values()
+    return jsonify([amenity.to_dict() for amenity in all_amenities])
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+def get_amenity(amenity_id):
+    """Retrieves a Amenity object"""
+    amenity = storage.get('Amenity', amenity_id)
+    if amenity is None:
+        abort(404)
+    return jsonify(amenity.to_dict())
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+def delete_amenity(amenity_id):
+    """Deletes a Amenity object"""
+    amenity = storage.get('Amenity', amenity_id)
+    if amenity is None:
+        abort(404)
+    storage.delete(amenity)
+   return jsonify({}), 200
+
