b3598944e0be88b11e6a0cfea97b2722bc1cfe25
Jenna Jones <jljones121@gmail.com>
2024-07-16 15:55:58 -0500

    pep8 fixes places.py

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -7,7 +7,9 @@ from models.place import Place
 from models.city import City
 from models.user import User
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/cities/<city_id>/places',
+                 methods=['GET'], strict_slashes=False)
 def get_places(city_id):
     """ Retrieves the list of all Place objects of a City """
     print(f"Retrieving places for city_id: {city_id}")
@@ -17,6 +19,7 @@ def get_places(city_id):
         abort(404)
     places = [place.to_dict() for place in city.places]
     return jsonify(places)
+
 
 @app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
 def get_place(place_id):
@@ -28,7 +31,9 @@ def get_place(place_id):
         abort(404)
     return jsonify(place.to_dict())
 
-@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/places/<place_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_place(place_id):
     """ Deletes a Place object """
     print(f"Deleting place with place_id: {place_id}")
@@ -41,7 +46,9 @@ def delete_place(place_id):
     print(f"Place {place_id} deleted successfully")
     return jsonify({}), 200
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
+
+@app_views.route('/cities/<city_id>/places',
+                 methods=['POST'], strict_slashes=False)
 def create_place(city_id):
     """ Creates a Place """
     print(f"Creating place in city with city_id: {city_id}")
@@ -69,6 +76,7 @@ def create_place(city_id):
     place.save()
     print(f"Place created with data: {data}")
     return jsonify(place.to_dict()), 201
+
 
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
 def update_place(place_id):
