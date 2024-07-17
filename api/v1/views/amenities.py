ed3d80c08ca5864edc41f7f493f3358bb67525d0
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-17 00:49:29 +0000

    reworked.

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -73,11 +73,9 @@ from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
 from models.amenity import Amenity
-from flasgger.utils import swag_from
 
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/amenity/get.yml', methods=['GET'])
 def get_all_amenities():
     """ get amenities by id """
     all_list = [obj.to_dict() for obj in storage.all(Amenity).values()]
@@ -86,7 +84,6 @@ def get_all_amenities():
 
 @app_views.route('/amenities/<string:amenity_id>', methods=['GET'],
                  strict_slashes=False)
-@swag_from('documentation/amenity/get_id.yml', methods=['GET'])
 def get_amenity(amenity_id):
     """ get amenity by id"""
     amenity = storage.get(Amenity, amenity_id)
@@ -97,7 +94,6 @@ def get_amenity(amenity_id):
 
 @app_views.route('/amenities/<string:amenity_id>', methods=['DELETE'],
                  strict_slashes=False)
-@swag_from('documentation/amenity/delete.yml', methods=['DELETE'])
 def del_amenity(amenity_id):
     """ delete amenity by id"""
     amenity = storage.get(Amenity, amenity_id)
@@ -110,22 +106,20 @@ def del_amenity(amenity_id):
 
 @app_views.route('/amenities/', methods=['POST'],
                  strict_slashes=False)
-@swag_from('documentation/amenity/post.yml', methods=['POST'])
 def create_obj_amenity():
     """ create new instance """
-    if not request.get_json():
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
-    if 'name' not in request.get_json():
-        return make_response(jsonify({"error": "Missing name"}), 400)
-    js = request.get_json()
-    obj = Amenity(**js)
-    obj.save()
-    return (jsonify(obj.to_dict()), 201)
+    if not request.is_json:
+        return (jsonify({"error": "Not a JSON"}), 400)
+    data = request.get_json()
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    amenity = Amenity(**data)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 201
 
 
 @app_views.route('/amenities/<string:amenity_id>', methods=['PUT'],
                  strict_slashes=False)
-@swag_from('documentation/amenity/put.yml', methods=['PUT'])
 def post_amenity(amenity_id):
     """  """
     if not request.get_json():
