3e492cd0164762b0a4709c6eed37aad94d036458
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-17 00:50:46 +0000

    rework put method.

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -118,16 +118,17 @@ def create_obj_amenity():
     return jsonify(amenity.to_dict()), 201
 
 
-@app_views.route('/amenities/<string:amenity_id>', methods=['PUT'],
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
                  strict_slashes=False)
-def post_amenity(amenity_id):
-    """  """
-    if not request.get_json():
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
-    obj = storage.get(Amenity, amenity_id)
-    if obj is None:
+def put_amenity(amenity_id):
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
         abort(404)
-    for key, value in request.get_json().items():
+    if not request.is_json:
+        abort(400, 'Not a JSON')
+    data = request.get_json()
+    for key, value in data.items():
         if key not in ['id', 'created_at', 'updated_at']:
-            setattr(obj, key, value)
+            setattr(amenity, key, value)
     storage.save()
+    return jsonify(amenity.to_dict()), 200
