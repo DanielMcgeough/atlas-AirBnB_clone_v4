57b9e13e56b2dd9ad58799e50208852b95290df1
Jenna Jones <jljones121@gmail.com>
2024-07-16 22:54:13 -0500

    added pool_size and max_overflow to resolve connection pool error

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -36,11 +36,14 @@ class DBStorage:
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
         HBNB_ENV = getenv('HBNB_ENV')
+        
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
                                       format(HBNB_MYSQL_USER,
                                              HBNB_MYSQL_PWD,
                                              HBNB_MYSQL_HOST,
-                                             HBNB_MYSQL_DB))
+                                             HBNB_MYSQL_DB),
+                                      pool_size=10, max_overflow=20)
+
         if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
