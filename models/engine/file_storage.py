f150a46f0b050cc2d53d45cf1288482109c1a402
Jenna Jones <jljones121@gmail.com>
2024-07-16 22:08:18 -0500

    whitespace file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,7 +62,7 @@ class FileStorage:
             key = obj.to_dict()['__class__'] + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
-           
+
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
