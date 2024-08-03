3e2cebe689e0f4c4f93c5e5e18b9a6efc7124dbb
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-08-03 10:40:57 -0500

    More incoming changes.

--- models/city.py
+++ models/city.py
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python
-""" holds class City"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-
-
-class City(BaseModel, Base):
-    """Representation of city """
-    if models.storage_t == "db":
-        __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        places = relationship("Place",
-                              backref="cities",
-                              cascade="all, delete, delete-orphan")
-    else:
-        state_id = ""
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes city"""
-        super().__init__(*args, **kwargs)
-=======
 #!/usr/bin/python
 """ holds class City"""
 import models
@@ -50,4 +22,3 @@ class City(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes city"""
         super().__init__(*args, **kwargs)
->>>>>>> 8ccf1ad752188b67c8e49134b6a766b15f525575
--- models/user.py
+++ models/user.py
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-""" holds class User"""
-
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from hashlib import md5
-
-
-class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
-
-    def __setattr__(self, name, value):
-        """sets a password with md5 encryption"""
-        if name == "password":
-            value = md5(value.encode()).hexdigest()
-        super().__setattr__(name, value)
-=======
 #!/usr/bin/python3
 """ holds class User"""
 import models
@@ -66,4 +27,3 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
->>>>>>> 8ccf1ad752188b67c8e49134b6a766b15f525575
