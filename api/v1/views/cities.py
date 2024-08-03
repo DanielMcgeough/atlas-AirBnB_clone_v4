5dd62ed7a38293bc0f30833e4f1e431c7b7a0cca
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-08-03 10:37:04 -0500

    the amount of incoming changes is staggering.

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,38 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-""" objects that handles all default RestFul API actions for cities """
-from models.city import City
-from models.state import State
-from models import storage
-from api.v1.views import app_views
-from flask import abort, jsonify, make_response, request
-from flasgger.utils import swag_from
-
-
-@app_views.route('/states/<state_id>/cities', methods=['GET'],
-                 strict_slashes=False)
-@swag_from('documentation/city/cities_by_state.yml', methods=['GET'])
-def get_cities(state_id):
-    """
-    Retrieves the list of all cities objects
-    of a specific State, or a specific city
-    """
-    list_cities = []
-    state = storage.get(State, state_id)
-    if not state:
-        abort(404)
-    for city in state.cities:
-        list_cities.append(city.to_dict())
-
-    return jsonify(list_cities)
-
-
-@app_views.route('/cities/<city_id>/', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/city/get_city.yml', methods=['GET'])
-def get_city(city_id):
-    """
-    Retrieves a specific city based on id
-=======
 """
 Creates new view for City obj that handles the restful API
 """
@@ -63,7 +29,6 @@ def get_cities_by_state(state_id):
 def get_city_by_id(city_id):
     """
     Retrieves a specific city by its ID
->>>>>>> 8ccf1ad752188b67c8e49134b6a766b15f525575
     """
     city = storage.get(City, city_id)
     if not city:
@@ -72,29 +37,6 @@ def get_city_by_id(city_id):
 
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
-<<<<<<< HEAD
-@swag_from('documentation/city/delete_city.yml', methods=['DELETE'])
-def delete_city(city_id):
-    """
-    Deletes a city based on id provided
-    """
-    city = storage.get(City, city_id)
-
-    if not city:
-        abort(404)
-    storage.delete(city)
-    storage.save()
-
-    return make_response(jsonify({}), 200)
-
-
-@app_views.route('/states/<state_id>/cities', methods=['POST'],
-                 strict_slashes=False)
-@swag_from('documentation/city/post_city.yml', methods=['POST'])
-def post_city(state_id):
-    """
-    Creates a City
-=======
 def delete_city(city_id):
     """
     Deletes a specific city by its ID
@@ -111,30 +53,10 @@ def delete_city(city_id):
 def post_city(state_id):
     """
     Creates a new city and stores it.
->>>>>>> 8ccf1ad752188b67c8e49134b6a766b15f525575
     """
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-<<<<<<< HEAD
-    if not request.get_json():
-        abort(400, description="Not a JSON")
-    if 'name' not in request.get_json():
-        abort(400, description="Missing name")
-
-    data = request.get_json()
-    instance = City(**data)
-    instance.state_id = state.id
-    instance.save()
-    return make_response(jsonify(instance.to_dict()), 201)
-
-
-@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
-@swag_from('documentation/city/put_city.yml', methods=['PUT'])
-def put_city(city_id):
-    """
-    Updates a City
-=======
     if not request.is_json:  # check for malformed request
         abort(400, 'Not a JSON')
     data = request.get_json()
@@ -151,25 +73,10 @@ def put_city(city_id):
 def put_city(city_id):
     """
     Updates a specific city by its ID
->>>>>>> 8ccf1ad752188b67c8e49134b6a766b15f525575
     """
     city = storage.get(City, city_id)
     if not city:
         abort(404)
-<<<<<<< HEAD
-
-    if not request.get_json():
-        abort(400, description="Not a JSON")
-
-    ignore = ['id', 'state_id', 'created_at', 'updated_at']
-
-    data = request.get_json()
-    for key, value in data.items():
-        if key not in ignore:
-            setattr(city, key, value)
-    storage.save()
-    return make_response(jsonify(city.to_dict()), 200)
-=======
     if not request.is_json:  # check for malformed request
         abort(400, 'Not a JSON')
     data = request.get_json()
@@ -180,4 +87,3 @@ def put_city(city_id):
             setattr(city, key, value)
     storage.save()
     return jsonify(city.to_dict()), 200
->>>>>>> 8ccf1ad752188b67c8e49134b6a766b15f525575
